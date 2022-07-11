08046f97070f4682cf373e68e379acec2d180f5c
zhongxm <zhongxiaoming@52tt.com>
2022-07-11 17:13:47 +0800

    自动同步(push)[at 2022-07-11 17:13:47]

--- batch_push.sh
+++ batch_push.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 
+echo "running script $0..."
+
+
 ############################## 公司机器
 # xcode setting
-push_xcode_setting() {
-	~/Library/Developer/Xcode/UserData/.syn_tools/git_push
-}
 
-# Android Studio
-push_android_studio_setting(){
-	cd ~/Library/Application\ Support/Google/AndroidStudio2021.2/.syn_tools
-	./git_push
-}
+push_xcode_setting=~/Library/Developer/Xcode/UserData
+push_android_studio_setting=~/Library/Application\ Support/Google/AndroidStudio2021.2
+batch_sh=~/.my_batch_sh
+
+# push_xcode_setting() {
+# 	~/Library/Developer/Xcode/UserData/.syn_tools/git_push
+# }
+
+# # Android Studio
+# push_android_studio_setting(){
+# 	cd ~/Library/Application\ Support/Google/AndroidStudio2021.2/.syn_tools
+# 	./git_push
+# }
 
 
-# batch_sh 批量同步命令（给crontab用）
-push_batch_sh(){
-	cd ~/.my_batch_sh
-	git add *
-	git rm $(git ls-files --deleted)
-	git commit -m "更新batch_sh脚本内容"
-	echo "git push..."
-	git push
-}
+# # batch_sh 批量同步命令（给crontab用）
+# push_batch_sh(){
+# 	cd ~/.my_batch_sh
+# 	git add *
+# 	git rm $(git ls-files --deleted)
+# 	git commit -m "更新batch_sh脚本内容"
+# 	echo "git push..."
+# 	git push
+# }
 
 
 
 main(){
-	push_batch_sh
-	push_xcode_setting
-	push_android_studio_setting
+
+	~/.my_syn_tools/.syn_tools/git_push "$batch_sh"
+	~/.my_syn_tools/.syn_tools/git_push "$push_xcode_setting"
+	~/.my_syn_tools/.syn_tools/git_push "$push_android_studio_setting"
 }
 
 
